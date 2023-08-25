c1d72686f8330ae121fba4059b05dfbe15e82b7e
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-08-25 09:29:35 +0000

    Comments added so I understand code better

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,18 +4,26 @@ from .models import Order
 
 class OrderForm(forms.ModelForm):
     class Meta:
+        # Here we are creating a form using the Order model
+        # And only using fields that are not automatically filled in
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
+    # Here we are overriding the normal init method
+
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
+        # This is the default init method
         super().__init__(*args, **kwargs)
+        # Here we have created a dictionary of form fields
+        # They will show up in the form fields rather than
+        # clunky field names and empty text boxes in template
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
@@ -28,7 +36,12 @@ class OrderForm(forms.ModelForm):
             'county': 'County',
         }
 
+        # This is just setting the focus automatically to where we want it
         self.fields['full_name'].widget.attrs['autofocus'] = True
+        # This adds stars to the required fields
+        # And sets revelant placeholder names to their inputs
+        # Also adds a css class
+        # And removes labels as we have placeholders
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
@@ -37,3 +50,4 @@ class OrderForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+        # This is advanced form customization!
--- checkout/signals.py
+++ checkout/signals.py
@@ -3,11 +3,24 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+# We have created this file to call the function in Order
+# That calculates the totals of items and updates the fields in the admin page
 
+
+# This handles signals from the post_save event
+# Sender is Order Line Item
+# Instance of the model that sent it, Order?
+# A boolean sent by django referring to whether this is a new instance or updated one
+# And kwargs "Key Word Arguements"
+
+# This is the event listener if you like, listening to saves on the Order Line Item model
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
+    # This uses the update total function on the order
     instance.order.update_total()
 
+# This calls the same update function when an item is deleted
 @receiver(post_delete, sender=OrderLineItem)
+# The created parameter is not sent by the "deleted" signal, no need to mention it
 def update_on_save(sender, instance, **kwargs):
     instance.order.update_total()
