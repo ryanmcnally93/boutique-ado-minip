bf3a92ef68dc16a4c9852d817a1148733e68508a
ryanmcnally93 <>
2023-08-28 17:02:40 +0000

    Complete Webhok Handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# This document adds fields to the admin page
 
-
-# Allows us to make changes to individual line items in the admin interface
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
@@ -13,26 +10,22 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    # These will be calculated by the model, not changed by admin
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
-    # Writing all the fields here isn't required
-    # But means we can structure the order of the attributes
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
-    # This restricts the columns that show up to only a few key items
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'grand_total',)
 
-    # Order items by date, most recent at the top
     ordering = ('-date',)
 
-# We don't need to register the line items as they are in the Order class
 admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -6,16 +6,8 @@ from django.conf import settings
 
 from products.models import Product
 
-# uuid will be used to generate order number
-
 
 class Order(models.Model):
-    """
-    These are the attribute of the whole order itself
-    Postcode and County aren't neccessary in every country, so they can be empty
-    auto_now_add, sets date and time to now
-    Order number is unique and unchangeable, so orders can be easily found
-    """
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -30,61 +22,55 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-    # The initial underscore means this function can only be used within this class
     def _generate_order_number(self):
-        # This will generate a random string of 32 characters as order number
+        """
+        Generate a random, unique order number using UUID
+        """
         return uuid.uuid4().hex.upper()
 
-    # Here we are using aggregate and sum to find the total of the line items
-    # We are also adding a new field to the query set called lineitem_total__sum
     def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        # Here we are calculating the delivery cost.
-        # Standard delivery is for number of items under the threshold amount
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
-        # And this is when the threshold is reached, free delivery!
             self.delivery_cost = 0
-        # From this we get the grand total and save
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-    # This function overrides the original save method
-    # It is checking that there is an order number, and if not, it runs the function
-    # Then it saves the same way it normally would
     def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
-    # This returns the order number in string format
     def __str__(self):
         return self.order_number
 
 
 class OrderLineItem(models.Model):
-    """
-    These are the attributes of individual order items
-    They have a relation to the order so that needs to be here
-    We can access this through order.lineitems, due to the related name field
-    The product is a foreign key so we can access the product model too
-    The sizes can be null due to some items not being clothes
-    line item total cannot be changed by user, but updates automatically
-    """
-
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
-    # The line item total needs to be timesed by the quantity correctly
     def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
-    # This returns the SKU and order number in string format
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
