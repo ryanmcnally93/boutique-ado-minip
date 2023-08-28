530486e38b18e859415e178d3998737276a07fa7
ryanmcnally93 <>
2023-08-28 17:18:50 +0000

    Update checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -28,12 +28,11 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # This is just setting the focus automatically to where we want it
@@ -43,11 +42,12 @@ class OrderForm(forms.ModelForm):
         # Also adds a css class
         # And removes labels as we have placeholders
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
         # This is advanced form customization!
