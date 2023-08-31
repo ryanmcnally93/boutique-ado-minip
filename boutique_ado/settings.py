0e012dd76c27dd7883d6e1389a4a3e02b1803201
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-08-31 14:29:06 +0000

    Fix verification email issue

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -214,5 +214,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-ACCOUNT_EMAIL_VERIFICATION = 'none'
